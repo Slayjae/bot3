c656916321cac8b4a4e08b0fbf9aefb14277e7fa
Slayjae <wallack.ajay.j@gmail.com>
2020-01-09 00:12:13 -0500

    Token error error fix

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const client = new Discord.Client();
 client.once('ready', () => {
 	console.log('Didn\'t crash instantly');
 });
-try{client.login(oops)}catch(err){
+try{client.login(TOKEN)}catch(err){
 console.error("TOKEN was incorrect/missing. By default it is currently in "+ path.resolve(__dirname,envFile) + ". This may be modified by changing \'envFile\' on line "+ stack.split(path.resolve(__dirname,'./index.js'))[1].split(":")[1]);
 }
